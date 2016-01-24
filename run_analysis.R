7ef982e58cc51eb1ba033373f8817ff3c7a70f4d
wil337 <wil.lee.am@gmail.com>
2016-01-24 19:18:05 +1100

    final
    final submission with clean up and tidy and commentary

--- run_analysis.R
+++ run_analysis.R
@@ -6,31 +6,53 @@
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 library(dplyr)
-#1. merge training and test sets
+library(tidyr)
+library(stringr)
+# import the training data and response variables
 datatrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt",header=F,sep="")
-vars<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",header=F,sep="")
-#4. labels
-
 resptrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt",header=F,sep="")
+
+#import the variable names
+vars<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",header=F,sep="")
 varsA<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt",header=F,sep="")
-#3. name activities
+
+#attach names to training dataset
+names(datatrain)<- vars$V2
 datatrain$activity<-factor(x=resptrain$V1,levels=c(1,2,3,4,5,6),labels=varsA$V2)
 subjecttrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt",header=F,sep="")
 datatrain$subject<-factor(subjecttrain$V1,levels=seq(1,30,1))
 
+
+#import the test data and attach names to test dataset
 datatest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt",header=F,sep="")
 resptest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt",header=F,sep="")
+names(datatest)<- vars$V2
 datatest$activity<-factor(x=resptest$V1,levels=c(1,2,3,4,5,6),labels=varsA$V2)
 subjecttest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt",header=F,sep="")
 datatest$subject<-factor(subjecttest$V1,levels=seq(1,30,1))
 
+#merge training and test datasets
 data<-bind_rows(datatrain,datatest)
-str(datatrain2)
 
-#2. mean and stdev
+#get the mean and stdev columns only (and the activity and subject variables)
 data2<-select(data,matches(".mean.|.std.|activity|subject"))
 
 
-#5. average per subject per activity, summary 
-tidy<-select(data2,-matches(".std.")) %>% group_by(activity,subject) %>% summarise_each(funs(mean))
+#dataset with average per subject per activity, summary 
+tidy<-select(data2,-contains("angle") )%>% group_by(activity,subject) %>% summarise_each(funs(mean))
 
+#start creating tidy datasets, with columns put into rows where relevat
+tidy2<-gather(tidy, key,value,-activity,-subject) %>%
+  mutate(dashCount=str_count(key,"-")) %>%
+  mutate(key2=ifelse(dashCount ==2,as.character(key),paste0(as.character(key),"-"))) #count number of dashes
+
+tidy3<-separate(tidy2,key2,into = c("action", "measure","coordinate"), sep="-") #separate the key variable into other varaibles
+tidy4<-separate(tidy3,action,into = c("t_f","action"),sep=1) %>% 
+  select(-c(key,dashCount))
+
+#final cleaning - convert to factors
+tidy4$t_f <- as.factor(tidy4$t_f)
+tidy4$action <- as.factor(tidy4$action)
+tidy4$coordinate <- as.factor(tidy4$coordinate)
+tidy4$measure <- as.factor(tidy4$measure)
+str(tidy4)
