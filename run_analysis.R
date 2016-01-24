81bf1a277bd594c9abe8b2d8900041f49ca95fb5
wil337 <wil.lee.am@gmail.com>
2016-01-24 16:56:11 +1100

    change1
    merging on subject and response (activity)

--- run_analysis.R
+++ run_analysis.R
@@ -5,21 +5,28 @@
 #4.Appropriately labels the data set with descriptive variable names. 
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
-data <- read.table(unz("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", "Sales.dat"), nrows=10, header=T, quote="\"", sep=",")
-?unz
-
-unzip<-unz("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip","/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt")
-train<- read.table(unzip,header=FALSE)
-close(unzip)
-
+library(dplyr)
 #1. merge training and test sets
 datatrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt",header=F,sep="")
 vars<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",header=F,sep="")
-names(datatrain)<- gsub("()","",vars$V2)
+
+resptrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt",header=F,sep="")
+varsA<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt",header=F,sep="")
+datatrain$activity<-factor(x=resptrain$V1,levels=c(1,2,3,4,5,6),labels=varsA$V2)
+subjecttrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt",header=F,sep="")
+datatrain$subject<-factor(subjecttrain$V1,levels=seq(1,30,1))
+
 datatest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt",header=F,sep="")
-names(datatest)<- gsub("()","",vars$V2)
+resptest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt",header=F,sep="")
+datatest$activity<-factor(x=resptest$V1,levels=c(1,2,3,4,5,6),labels=varsA$V2)
+subjecttest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt",header=F,sep="")
+datatest$subject<-factor(subjecttest$V1,levels=seq(1,30,1))
+
 data<-bind_rows(datatrain,datatest)
-names(data)
+str(datatrain2)
+
 #2. mean and stdev
-library(dplyr)
-data2<-select(data,matches(".mean.|.std"))
+data2<-select(data,matches(".mean.|.std."))
+
+#3. name activities
+names(data2)
