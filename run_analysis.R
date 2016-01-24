578786befee326c365e6b595d31cdf544ead5e96
wil337 <wil.lee.am@gmail.com>
2016-01-24 19:50:30 +1100

    final final final
    with the working directory at top

--- run_analysis.R
+++ run_analysis.R
@@ -5,30 +5,32 @@
 #4.Appropriately labels the data set with descriptive variable names. 
 #5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+#change working directory
+setwd("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset")
 library(dplyr)
 library(tidyr)
 library(stringr)
 # import the training data and response variables
-datatrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt",header=F,sep="")
-resptrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt",header=F,sep="")
+datatrain<-read.table("train/X_train.txt",header=F,sep="")
+resptrain<-read.table("train/y_train.txt",header=F,sep="")
 
 #import the variable names
-vars<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt",header=F,sep="")
-varsA<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt",header=F,sep="")
+vars<-read.table("features.txt",header=F,sep="")
+varsA<-read.table("activity_labels.txt",header=F,sep="")
 
 #attach names to training dataset
 names(datatrain)<- vars$V2
 datatrain$activity<-factor(x=resptrain$V1,levels=c(1,2,3,4,5,6),labels=varsA$V2)
-subjecttrain<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt",header=F,sep="")
+subjecttrain<-read.table("train/subject_train.txt",header=F,sep="")
 datatrain$subject<-factor(subjecttrain$V1,levels=seq(1,30,1))
 
 
 #import the test data and attach names to test dataset
-datatest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt",header=F,sep="")
-resptest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt",header=F,sep="")
+datatest<-read.table("test/X_test.txt",header=F,sep="")
+resptest<-read.table("test/y_test.txt",header=F,sep="")
 names(datatest)<- vars$V2
 datatest$activity<-factor(x=resptest$V1,levels=c(1,2,3,4,5,6),labels=varsA$V2)
-subjecttest<-read.table("C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt",header=F,sep="")
+subjecttest<-read.table("test/subject_test.txt",header=F,sep="")
 datatest$subject<-factor(subjecttest$V1,levels=seq(1,30,1))
 
 #merge training and test datasets
@@ -56,4 +58,4 @@ tidy4$action <- as.factor(tidy4$action)
 tidy4$coordinate <- as.factor(tidy4$coordinate)
 tidy4$measure <- as.factor(tidy4$measure)
 str(tidy4)
-write.table(tidy4,"C:/Users/William/Box Sync/Coursera/Getting Cleaning Data/GettingCleaningData/tidydata.txt",row.names=FALSE)
+write.table(tidy4,"tidydata.txt",row.names=FALSE)
